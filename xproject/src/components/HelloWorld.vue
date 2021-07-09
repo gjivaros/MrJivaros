b45534728f5373d326a0de008c61a7b254a45956
MrJivaros <jivarosdev@gmail.com>
2021-07-09 16:39:15 +0200

    init project

---
+++ xproject/src/components/HelloWorld.vue
@@ -0,0 +1,595 @@
+<template>
+  <!-- About menu -->
+  <div class="relative bg-white overflow-hidden">
+    <div class="max-w-7xl mx-auto">
+      <div
+        class="
+          relative
+          z-10
+          pb-8
+          bg-white
+          sm:pb-16
+          md:pb-20
+          lg:max-w-2xl
+          lg:w-full
+          lg:pb-28
+          xl:pb-32
+        "
+      >
+        <main
+          class="
+            mt-10
+            mx-auto
+            max-w-7xl
+            px-4
+            sm:mt-12
+            sm:px-6
+            md:mt-16
+            lg:mt-20
+            lg:px-8
+            xl:mt-28
+          "
+        >
+          <div class="sm:text-center lg:text-left">
+            <h1
+              class="
+                text-4xl
+                tracking-tight
+                font-extrabold
+                text-gray-900
+                sm:text-3xl
+                md:text-6xl
+              "
+            >
+              <span class="block xl:text-5xl">Achetez et vendez</span>
+              <span class="block xl:text-5xl">
+                de la monnaie électronique
+              </span>
+              <span
+                class="
+                  block
+                  xl:text-5xl xl:text-center
+                  text-green-600
+                  xl:inline
+                "
+                >librement.</span
+              >
+            </h1>
+            <p
+              class="
+                mt-3
+                text-base text-gray-500
+                sm:mt-5
+                sm:text-lg
+                sm:max-w-xl
+                sm:mx-auto
+                md:mt-5
+                md:text-xl
+                lg:mx-0
+              "
+            >
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua.
+            </p>
+            <div
+              class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"
+            >
+              <div class="rounded-md shadow">
+                <a
+                  href="#"
+                  class="
+                    w-full
+                    flex
+                    items-center
+                    justify-center
+                    md:justify-between
+                    px-8
+                    py-3
+                    border border-transparent
+                    text-base
+                    font-medium
+                    rounded-md
+                    text-white
+                    bg-green-600
+                    hover:bg-green-700
+                    md:py-4
+                    md:text-lg
+                    md:px-10
+                  "
+                >
+                  <p>Envoyer</p>
+                  <span><i class="fas fa-paper-plane"></i> </span>
+                </a>
+              </div>
+              <div class="mt-3 sm:mt-0 sm:ml-3">
+                <a
+                  href="#"
+                  class="
+                    w-full
+                    flex
+                    items-center
+                    justify-center
+                    px-8
+                    py-3
+                    border border-transparent
+                    text-base
+                    font-medium
+                    rounded-md
+                    text-green-700
+                    bg-green-100
+                    hover:bg-green-200
+                    md:py-4
+                    md:text-lg
+                    md:px-10
+                  "
+                >
+                  <p>Recevoir</p>
+                  <i class="fad fa-arrow-alt-up"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <img
+        class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+        src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+        alt=""
+      />
+    </div>
+  </div>
+
+  <!-- Presentation des types d'echande -->
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <div class="py-12 bg-white">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="lg:text-center">
+        <div class="mt-3 sm:mt-0 sm:ml-3">
+          <p
+            href="#"
+            class="
+              w-full
+              flex
+              items-center
+              justify-center
+              px-8
+              py-3
+              border border-transparent
+              text-base
+              font-medium
+              rounded-md
+              text-green-700
+              bg-green-100
+              hover:bg-green-200
+              md:py-4
+              md:text-lg
+              md:px-10
+            "
+          >
+            Transactions
+          </p>
+        </div>
+
+        <p
+          class="
+            mt-2
+            text-3xl
+            leading-8
+            font-extrabold
+            tracking-tight
+            text-gray-900
+            sm:text-4xl
+          "
+        >
+          Une meilleure façon d'envoyer de l'argent
+        </p>
+        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+          Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+          voluptatum cupiditate veritatis in accusamus quisquam.
+        </p>
+      </div>
+
+      <div class="mt-10">
+        <dl
+          class="
+            space-y-10
+            md:space-y-0
+            md:grid md:grid-cols-2
+            md:gap-x-8 md:gap-y-10
+          "
+        >
+          <div class="relative">
+            <dt>
+              <div
+                class="
+                  absolute
+                  flex
+                  items-center
+                  justify-center
+                  h-12
+                  w-12
+                  rounded-md
+                  bg-gray-300
+                  text-white
+                "
+              >
+                <!-- Heroicon name: outline/globe-alt -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                  />
+                </svg>
+              </div>
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                Dollar Bitcoin
+              </p>
+            </dt>
+            <dd class="mt-2 ml-16 text-base text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+              impedit perferendis suscipit eaque, iste dolor cupiditate
+              blanditiis ratione.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <div
+                class="
+                  absolute
+                  flex
+                  items-center
+                  justify-center
+                  h-12
+                  w-12
+                  rounded-md
+                  bg-yellow-900
+                  text-white
+                "
+              >
+                <!-- Heroicon name: outline/scale -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                  />
+                </svg>
+              </div>
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                Dollar Payeer
+              </p>
+            </dt>
+            <dd class="mt-2 ml-16 text-base text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+              impedit perferendis suscipit eaque, iste dolor cupiditate
+              blanditiis ratione.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <div
+                class="
+                  absolute
+                  flex
+                  items-center
+                  justify-center
+                  h-12
+                  w-12
+                  rounded-md
+                  bg-red-300
+                  text-white
+                "
+              >
+                <!-- Heroicon name: outline/lightning-bolt -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13 10V3L4 14h7v7l9-11h-7z"
+                  />
+                </svg>
+              </div>
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                Dollar Perfect Money
+              </p>
+            </dt>
+            <dd class="mt-2 ml-16 text-base text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+              impedit perferendis suscipit eaque, iste dolor cupiditate
+              blanditiis ratione.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <div
+                class="
+                  absolute
+                  flex
+                  items-center
+                  justify-center
+                  h-12
+                  w-12
+                  rounded-md
+                  bg-yellow-300
+                  text-white
+                "
+              >
+                <!-- Heroicon name: outline/annotation -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                  />
+                </svg>
+              </div>
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                MTN Mobile Money
+              </p>
+            </dt>
+            <dd class="mt-2 ml-16 text-base text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+              impedit perferendis suscipit eaque, iste dolor cupiditate
+              blanditiis ratione.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <div
+                class="
+                  absolute
+                  flex
+                  items-center
+                  justify-center
+                  h-12
+                  w-12
+                  rounded-md
+                  bg-blue-700
+                  text-white
+                "
+              >
+                <!-- Heroicon name: outline/annotation -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                  />
+                </svg>
+              </div>
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                MOOV Mobile Money
+              </p>
+            </dt>
+            <dd class="mt-2 ml-16 text-base text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+              impedit perferendis suscipit eaque, iste dolor cupiditate
+              blanditiis ratione.
+            </dd>
+          </div>
+
+          <div class="relative">
+            <dt>
+              <div
+                class="
+                  absolute
+                  flex
+                  items-center
+                  justify-center
+                  h-12
+                  w-12
+                  rounded-md
+                  bg-yellow-700
+                  text-white
+                "
+              >
+                <!-- Heroicon name: outline/annotation -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                  />
+                </svg>
+              </div>
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                ORANGE Money
+              </p>
+            </dt>
+            <dd class="mt-2 ml-16 text-base text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
+              impedit perferendis suscipit eaque, iste dolor cupiditate
+              blanditiis ratione.
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  </div>
+
+  <!-- pied de page -->
+  <footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="py-12 bg-white">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="mt-10">
+          <dl
+            class="
+              space-y-10
+              md:space-y-0
+              md:grid md:grid-cols-2
+              md:gap-x-8 md:gap-y-10
+            "
+          >
+            <div class="relative">
+              <dt>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  A propos de nous
+                </p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                <li>Qui sommes-nous?</li>
+                <li>Frais de service</li>
+                <li>Validateur de billets</li>
+                <li>Contact</li>
+              </dd>
+            </div>
+
+
+            <div class="relative">
+              <dt>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  En savoir plus
+                </p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                <li>Rejoignez-Nous</li>
+                <li>Question frequente</li>
+                <li>Condition d'utilisation</li>
+                <li>Politique et confidentialité</li>
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <div class="flex">
+  <div class="flex-none w-48 relative">
+    <img src="../assets/pexels-pixabay-315788.jpg" alt="" class="absolute inset-0 w-full h-full object-cover" />
+  </div>
+  <form class="flex-auto p-6">
+    <div class="flex flex-wrap">
+      <h1 class="flex-auto text-xl font-semibold">
+        Explorez nos prix exceptionnels
+      </h1>
+      <div class="text-xl font-semibold text-gray-500">
+        $110 000 000.00
+      </div>
+      <div class="w-full flex-none text-sm font-medium text-gray-500 mt-2">
+        En stock
+      </div>
+    </div>
+    <div class="flex items-baseline mt-4 mb-6">
+      <div class="space-x-2 flex">
+        <label>
+          <input class="w-9 h-9 flex items-center justify-center bg-green-100 rounded-lg" name="size" type="radio" value="xs" checked>
+          Dollar
+        </label>
+        <label>
+          <input class="w-9 h-9 flex items-center justify-center" name="size" type="radio" value="s">
+          Bitcoin
+        </label>
+        <label>
+          <input class="w-9 h-9 flex items-center justify-center" name="size" type="radio" value="m">
+          Perfect
+        </label>
+        <label>
+          <input class="w-9 h-9 flex items-center justify-center" name="size" type="radio" value="l">
+          Moov 
+        </label>
+        <label>
+          <input class="w-9 h-9 flex items-center justify-center" name="size" type="radio" value="xl">
+          Mtn
+        </label>
+      </div>
+      <div class="ml-auto text-sm text-gray-500 underline">Guide</div>
+    </div>
+    <div class="flex space-x-3 mb-4 text-sm font-medium">
+      <div class="flex-auto flex space-x-3">
+        <button class="w-1/2 flex items-center justify-center rounded-md bg-green-500 text-white" type="submit">Envoyer </button>
+        <button class="w-1/2 flex items-center justify-center rounded-md border border-gray-300" type="button">Recevoir</button>
+      </div>
+      <button class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-gray-400 border border-gray-300" type="button" aria-label="like">
+        <svg width="20" height="20" fill="currentColor">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+        </svg>
+      </button>
+    </div>
+    <p class="text-sm text-gray-500">
+      Tout droit reservé à {{AGENCE_NAME}}.
+    </p>
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data: () => {
+    return {
+      AGENCE_NAME: "Nom de l'agence",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@media screen and(min-width:5px) and (max-width: 600px) {
+  .home-title {
+    font-size: 1.5em;
+  }
+}
+</style>
